a936fac3c774446c04e8257424d5b4021a9d742d
Farhan Brillan <nyannss@proton.me>
2024-05-18 22:56:31 +0700

    delete all unecessary files

--- src/app/page.tsx
+++
@@ -1,7 +0,0 @@
-export default function Home() {
-  return (<main className="flex min-h-screen flex-col items-center justify-between p-24 bg-black">
-      <p className='text-white'>I'm farhan brillan</p>
-      <p className='text-white'>coming soon</p>
-    </main>);
-
-}
